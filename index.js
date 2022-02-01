72cd9f29ce0533089faaf19bbc9b0ec4b4265f93
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-01-31 22:51:32 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+function returnFirstTwoDrivers() {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers() {
+  return drivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiply) {
+  return function (fare) {
+    return fare * multiply;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, newer) {
+  return newer(drivers);
+}
