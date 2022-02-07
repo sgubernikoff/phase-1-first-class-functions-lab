d730cbcdc3cc55fdd09ab6d0d8aa0656d1dc362c
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-02-07 09:57:29 -0500

    commit tree 64e87d40fa750ee5b559bdf0aa976d97012c0146

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 // Code your solution in this file!
 const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
-function returnFirstTwoDrivers() {
+// function returnFirstTwoDrivers() {
+//   return drivers.slice(0, 2);
+// }
+
+// function returnLastTwoDrivers() {
+//   return drivers.slice(2);
+// }
+
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// function createFareMultiplier(multiply) {
+//   return function (fare) {
+//     return fare * multiply;
+//   };
+// }
+
+// const fareDoubler = createFareMultiplier(2);
+// const fareTripler = createFareMultiplier(3);
+
+// function selectDifferentDrivers(drivers, newer) {
+//   return newer(drivers);
+// }
+
+// // selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+// function pageLoad() {
+//   console.log("Page has loaded, run the program");
+//   return "this is page";
+// }
+// pageLoad();
+function returnFirstTwoDrivers(drivers) {
   return drivers.slice(0, 2);
 }
 
-function returnLastTwoDrivers() {
+function returnLastTwoDrivers(drivers) {
   return drivers.slice(2);
 }
 
@@ -19,6 +48,6 @@ function createFareMultiplier(multiply) {
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
-function selectDifferentDrivers(drivers, newer) {
-  return newer(drivers);
+function selectDifferentDrivers(driver, cb) {
+  return cb(driver);
 }
